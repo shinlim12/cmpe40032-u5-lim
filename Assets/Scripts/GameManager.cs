7775a4e99760425f11909e0b4d922cd4a5c23cf0
Shin I. Lim <92615780+ShinayLim@users.noreply.github.com>
2023-01-22 22:45:23 +0800

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
+    public bool isGameActive;
+
+    private float spawnRate = 1.0f;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void StartGame(int difficulty)
+    {
+        titleScreen.gameObject.SetActive(false);
+        spawnRate = spawnRate / difficulty;
+
+        isGameActive = true;
+
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# UnityLearn-Create_with_Code-Prototype_5
+Unit 5 - User Interface
+
+[![Post Card](https://tistory-readme-stats.vercel.app/api?name=myoung-min&postId=142&color=dark)](https://myoung-min.tistory.com/142)
